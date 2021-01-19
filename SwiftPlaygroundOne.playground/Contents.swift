a8d1a612f619fbb964f8a0bdfd9ba70c8234ff6a
NoahNua <NoahNua@hawaii.edu>
2021-01-19 10:37:17 -1000

    Add Variables to playground Class 01/19/21

--- SwiftPlaygroundOne.playground/Contents.swift
+++ SwiftPlaygroundOne.playground/Contents.swift
@@ -1,3 +1,38 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//declare a variable, name it, myfirstvariable and type it as a String
+var myFirstVariable: String
+
+//String is a data type that is repersented by a series of charecters. The string must be surrounded by ""
+
+//assign myFirstVariable to a tring
+myFirstVariable = "I've created my first string"
+
+//String Concatenation - combining strings together
+
+//declare and assign a seconed varialbe all in one line
+var  mySeconedVariable = " and look i combined 2 strings together"
+
+//create a third variable to hold the string concatenation
+
+var myThirdVariable = myFirstVariable + mySeconedVariable
+
+print(myThirdVariable)
+
+var  variableone: String
+
+variableone = "I love Animals"
+
+var variableTwo = ", Dogs are my favorite."
+
+var variableThree = " I own Two Dogs. "
+
+var variablefour  = variableone + variableTwo + variableThree
+
+print(variablefour)
+
+// adding spaces to following strings can add spaces when printing
+
+
